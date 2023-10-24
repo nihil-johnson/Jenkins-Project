80e2d166e18a1b398d9097936cbe093860a163b2
nihil-johnson <johnsonnihil2@gmail.com>
2023-10-24 20:26:30 +0530

    Making one error

--- First.java
+++ First.java
@@ -1,8 +1,8 @@
 public class First{
 	public static void main(String args[]){
 		System.out.println("This is first");
-		System.out.println("Run in Jenkins");
-		System.out.println("Git push Check");
+		System.out.println("Run in Jenkins")
+		System.out.println("Git push Check")
 		
 	}
 }
