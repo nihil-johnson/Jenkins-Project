685873b3985dc7533a41a5de1174c1013d068830
nihil-johnson <133639346+nihil-johnson@users.noreply.github.com>
2024-07-07 10:23:12 +0530

    small changes

--- First.java
+++ First.java
@@ -1,6 +1,7 @@
 public class First{
 	public static void main(String args[]){
 		System.out.println("This is first");
-		System.out.println("Run in Jenkins");		
+		System.out.println("Run in Jenkins");
+		System.out.println("Checking");	
 	}
 }
